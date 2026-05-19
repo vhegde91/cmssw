d6d8ebdb48731f9f409fd069c162ac8562cfc6d8
Vinay <vinay.hegde@cern.ch>
2026-05-19 16:28:34 +0200

    code-checks

--- RecoLocalCalo/HcalRecAlgos/src/HBHERun3Flags.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HBHERun3Flags.cc
@@ -1,6 +1,6 @@
 /*
-Identify bad recHits based on ADC values and capID misalignment.
-Relevant for Run 3 HBHE channels which use QIE11.
+  Identify bad recHits based on ADC values and capID misalignment.
+  Relevant for Run 3 HBHE channels which use QIE11.
 */
 #include "RecoLocalCalo/HcalRecAlgos/interface/HBHERun3Flags.h"
 #include <cstdint>
@@ -12,7 +12,8 @@ HBHERun3Flags::HBHERun3Flags() {}
 
 HBHERun3Flags::~HBHERun3Flags() {}
 
-bool HBHERun3Flags::isStuckADC(const QIE11DataFrame& digi) {  // All ADC values are the same and are above some threshold
+// All ADC values are the same and are above some threshold
+bool HBHERun3Flags::isStuckADC(const QIE11DataFrame& digi) {
   uint8_t nSamples = digi.samples();
   if (nSamples < 2)
     return false;
@@ -27,7 +28,8 @@ bool HBHERun3Flags::isStuckADC(const QIE11DataFrame& digi) {  // All ADC values 
   return true;
 }
 
-bool HBHERun3Flags::repeatedADCblock(const QIE11DataFrame& digi, const int soi) {  //looks for patterns like A B C D A B C D. ACD value should be > threshold in soi. Requires exaclty 8 samples
+//looks for patterns like A B C D A B C D. ACD value should be > threshold in soi. Requires exaclty 8 samples
+bool HBHERun3Flags::repeatedADCblock(const QIE11DataFrame& digi, const int soi) {
   if (isStuckADC(digi))
     return false;
 
@@ -37,7 +39,7 @@ bool HBHERun3Flags::repeatedADCblock(const QIE11DataFrame& digi, const int soi) 
 
   if (soi >= nSamples || digi[soi].adc() <= repeatedADCblock_min_)
     return false;
-  
+
   int nHalf = nSamples / 2;
   for (int i = 0; i < nHalf; i++) {
     if (digi[i].adc() != digi[i + nHalf].adc())
@@ -46,11 +48,10 @@ bool HBHERun3Flags::repeatedADCblock(const QIE11DataFrame& digi, const int soi) 
   return true;
 }
 
-bool HBHERun3Flags::isBadCapId(
-    const QIE11DataFrame& digi,
-    const int soi,
-    const uint32_t bx) {  //Does not pass (capId - bunchCrossing)%nCapsQIE11_ = a known value for the soi, where nCapsQIE11_ = 4
-  if (soi >= digi.samples()) return false;
+//Does not pass (capId - bunchCrossing)%nCapsQIE11_ = a known value for the soi, where nCapsQIE11_ = 4
+bool HBHERun3Flags::isBadCapId(const QIE11DataFrame& digi, const int soi, const uint32_t bx) {
+  if (soi >= digi.samples())
+    return false;
 
   short this_capidmbx = (digi[soi].capid() - bx) % nCapsQIE11_;
   if (this_capidmbx < 0)
@@ -59,14 +60,13 @@ bool HBHERun3Flags::isBadCapId(
   return (expCapIdInSOI_ != this_capidmbx);
 }
 
-bool HBHERun3Flags::nonRotatingCapId(
-    const QIE11DataFrame& digi,
-    const int soi,
-    const uint32_t bx) {  //Does not pass (capId - bunchCrossing)%nCapsQIE11_ = a known value for at least one TS
+//Does not pass (capId - bunchCrossing)%nCapsQIE11_ = a known value for at least one TS
+bool HBHERun3Flags::nonRotatingCapId(const QIE11DataFrame& digi, const int soi, const uint32_t bx) {
   uint8_t nSamples = digi.samples();
 
-  if (soi >= nSamples) return false;
-  
+  if (soi >= nSamples)
+    return false;
+
   for (int i = 0; i < nSamples; i++) {
     short this_capidmbx = (digi[i].capid() - bx) % nCapsQIE11_;
     if (this_capidmbx < 0)
